6d7676286cea8b933ed11d7b047a68e82921ca2f
nitish9413 <nitishkatkade94@gmail.com>
2023-06-26 16:01:03 +0530

    just

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,6 +1,7 @@
 import { g, auth, config } from '@grafbase/sdk'
 import Email from 'next-auth/providers/email'
 
+// @ts-ignore
 const User = g.model('User', {
   name: g.string().length({min:2,max:20}),
   email: g.string().unique(),
@@ -9,8 +10,10 @@ const User = g.model('User', {
   githubUrl: g.url().optional(),
   linkedinUrl: g.url().optional(),
   projects: g.relation(()=>Project).list().optional(),
-})
 
+}).auth((rules)=>{rules.public().read()})
+
+// @ts-ignore
 const Project=g.model('Project',{
   title:g.string().length({min:3}),
   description:g.string(),
@@ -19,8 +22,20 @@ const Project=g.model('Project',{
   githubUrl:g.url(),
   category:g.string().search(),
   createdBy:g.relation(()=>User)
+}).auth((rules)=>{
+  rules.public().read(),
+  rules.private().create().update().delete()
+})
+
+const jwt = auth.JWT({
+  issuer: 'grafbase',
+  secret: g.env('NEXTAUTH_SECRET'),
 })
 
 export default config({
-  schema: g
+  schema: g,
+  auth: {
+    providers: [jwt],
+    rules: (rules)=> rules.private()
+  },
 })
